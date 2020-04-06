76856c171d05558b4b2f55bf0845660a5d985c5e
White Joan <joannnabiala@gmail.com>
2020-04-06 21:36:44 +0200

    LW-7: Add welcome page

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import WelcomePage from "./WelcomePage";
+
+function App() {
+  return (
+    <div>
+      <WelcomePage />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/WelcomePage.js
@@ -0,0 +1,135 @@
+import React from 'react';
+import Logo from './img/logo.png'
+import bg0 from './img/bg-6.jpg'
+import bg1 from './img/bg-4.jpg'
+import bg2 from './img/bg-5.jpg'
+
+export default class MainPage extends React.Component {
+  render() {
+    return (
+      // header
+      <div className="layout">
+        <nav className="navbar navbar-expand-md navbar-light sticky-top">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="#">
+              <img alt="Let's work" src={Logo} className="img-fluid"/>
+            </a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
+            <span className="navbar-toggler-icon">
+            </span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarResposive">
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item active">
+                  <a className="btn btn-primary btn-lg nav-link border-primary rounded-left" href="#">Załóż konto</a>
+                </li>
+                <li className="nav-item active">
+                  <a className="btn btn-primary btn-lg border-secondary rounded-right" href="#">Zaloguj się</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+
+        <nav id="slides" className="carousel slide" data-ride="carousel">
+          <ul className="carousel-indicators">
+            <li data-target="#slides" data-slide-to="0" className="active"/>
+            <li data-target="#slides" data-slide-to="1"/>
+            <li data-target="#slides" data-slide-to="2"/>
+          </ul>
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <img alt="bg 0" src={bg1} className="img-fluid"/>
+                <div className="carousel-caption">
+                  <h1 className="display-2">Let's work</h1>
+                  <h3> Znajdź pracę swoich marzeń</h3>
+                </div>
+            </div>
+            <div className="carousel-item">
+              <img alt="bg-3" src={bg0} className="img-fluid"/>
+            </div>
+            <div className="carousel-item">
+              <img alt="bg-5" src={bg2} className="img-fluid"/>
+            </div>
+          </div>
+        </nav>
+
+        <nav className="container-fluid">
+          <div className="row jumbotron">
+            <div className="col-xs-12 col-sm-12 col-md-9 col-lg-9 col-xl-10">
+              <p className="lead text-center">
+                W tym miejscu będą kategorie ogłoszeń
+              </p>
+            </div>
+            <div className="col-xs-12 col-sm-12 col-md-3 col-lg-3 col-xl-2"/>
+            <a href="#">
+              <button type="button" className="btn btn-outline-secondary btn-lg align-self-center">Pokaż więcej ogłoszeń</button>
+            </a>
+          </div>
+        </nav>
+
+        <nav className="container-fluid padding">
+          <div className="row welcome text-center">
+            <div className="col-12">
+              <h1 className="display-4">
+                <p> Znajdź ją z nami...</p>
+              </h1>
+            </div>
+
+          </div>
+        </nav>
+
+
+        <nav className="container-fluid padding">
+          <div className="row text-center padding">
+            <div className="col-xs-12 col-sm-6 col-md-4 col-xl-3">
+              <i className="fas fa-search fa-7x"/>
+              <h3>Znajdź</h3>
+              <p>Stosując filtry znajdź interesującą Cię ofertę</p>
+            </div>
+            <div className="col-xs-12 col-sm-6 col-md-3">
+              <i className="fas fa-envelope-square fa-7x"/>
+              <h3>Skontaktuj się</h3>
+              <p>Napisz do pracodawcy</p>
+            </div>
+            <div className="col-xs-12 col-sm-6 col-md-3">
+              <i className="far fa-handshake fa-7x"/>
+              <h3>Przejdź rekrutację</h3>
+              <p>Umów się na rozmowę</p>
+            </div>
+            <div className="col-xs-12 col-sm-6 col-md-3">
+              <i className="far fa-smile fa-7x"/>
+              <h3>Pracuj!</h3>
+              <p>Miej pracę o jakiej zawsze marzyłeś</p>
+            </div>
+
+          </div>
+          <hr className="my-4">
+          </hr>
+        </nav>
+
+        <footer>
+          <div className="container-fluid padding">
+            <div className="row text-center">
+              <div className="col-md-1">
+                <img src={Logo} alt="logo"/>
+                <hr className="dark" />
+                <p>
+                724478420
+                </p>
+                <p>
+                  joannnabiala@gmail.com
+                </p>
+              </div>
+            </div>
+            <div className="text-center">
+              <div className="dark" />
+              <p>©lets-work-pl.pl</p>
+            </div>
+          </div>
+        </footer>
+
+      </div>
+    );
+  }
+}
