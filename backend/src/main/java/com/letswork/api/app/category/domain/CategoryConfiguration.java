f2d1d31d2554c673ce9a8ba9911adec823d28869
RobertKrzywina <robert.krzywina@gmail.com>
2020-04-19 19:10:49 +0200

    LW-47: Add tests for category validation

--- backend/src/main/java/com/letswork/api/app/category/domain/CategoryConfiguration.java
+++ backend/src/main/java/com/letswork/api/app/category/domain/CategoryConfiguration.java
@@ -3,11 +3,20 @@ package com.letswork.api.app.category.domain;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.concurrent.ConcurrentHashMap;
+
 @Configuration
 class CategoryConfiguration {
 
     @Bean
     CategoryFacade categoryFacade(CategoryRepository repository) {
+        CategoryValidator validator = new CategoryValidator();
+        CategoryService service = new CategoryService(repository, validator);
+        return new CategoryFacade(service);
+    }
+
+    CategoryFacade categoryFacade(ConcurrentHashMap<String, CategoryEntity> db) {
+        CategoryInMemoryRepository repository = new CategoryInMemoryRepository(db);
         CategoryValidator validator = new CategoryValidator();
         CategoryService service = new CategoryService(repository, validator);
         return new CategoryFacade(service);
