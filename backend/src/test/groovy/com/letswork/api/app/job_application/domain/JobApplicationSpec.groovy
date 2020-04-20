829488f800d7ab737b062b85d5abcd1b26137f50
RobertKrzywina <robert.krzywina@gmail.com>
2020-04-20 23:25:31 +0200

    LW-28: Add validaiton tests when apply job application

--- backend/src/test/groovy/com/letswork/api/app/job_application/domain/JobApplicationSpec.groovy
+++ backend/src/test/groovy/com/letswork/api/app/job_application/domain/JobApplicationSpec.groovy
@@ -4,9 +4,11 @@ import com.letswork.api.app.advertisement.domain.AdvertisementEntity
 import com.letswork.api.app.advertisement.domain.AdvertisementFacade
 import com.letswork.api.app.advertisement.domain.AdvertisementService
 import com.letswork.api.app.job_application.domain.dto.CreateJobApplicationDto
+import com.letswork.api.app.job_application.domain.exceptions.InvalidJobApplicationException
 import org.mockito.Mockito
 import spock.lang.Shared
 import spock.lang.Specification
+import spock.lang.Unroll
 
 import java.time.LocalDateTime
 import java.util.concurrent.ConcurrentHashMap
@@ -45,5 +47,40 @@ class JobApplicationSpec extends Specification {
 
         then: 'system has this job application'
         db.size() == 1
+    }
+
+    @Unroll
+    def 'Should throw an exception cause message is null, empty or blank = [#message]'(String message) {
+        when: 'we apply job application'
+        jobApplicationFacade.add(new CreateJobApplicationDto(message, 1L), 'tom@mail.com')
+
+        then: 'exception is thrown'
+        InvalidJobApplicationException exception = thrown()
+        exception.message == InvalidJobApplicationException.CAUSE.MESSAGE_EMPTY.message
+
+        where:
+        message | _
+        null    | _
+        ''      | _
+        '  '    | _
+    }
+
+    def 'Should throw an exception cause message length equals more than permissible length'() {
+        when: 'we apply job application'
+        String tooLongMessage = prepareMoreThan1000CharactersMessageLength()
+        jobApplicationFacade.add(new CreateJobApplicationDto(tooLongMessage, 1L), 'tom@mail.com')
+
+        then: 'exception is thrown'
+        InvalidJobApplicationException exception = thrown()
+        exception.message == InvalidJobApplicationException.CAUSE.MESSAGE_WRONG_LENGTH.message
+    }
+
+    private static String prepareMoreThan1000CharactersMessageLength() {
+        String tooLongMessage = 'thisMessageIsAbsolutelyTooLongForEveryAdvertisementYouWantToAdd'
+        StringBuilder sb = new StringBuilder()
+        for (int i = 0; i < 10; i++) {
+            sb.append(tooLongMessage)
+        }
+        return sb.toString()
     }
 }
